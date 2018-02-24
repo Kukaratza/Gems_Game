b07fd1ecadb690aeb93cda594748ee17bfa60094
Kukaratza <marioandres.martinez80@gmail.com>
2018-02-23 21:40:02 -0800

    working on the disable.onclick event after win/loss

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,7 +1,5 @@
-
-let totalScore = 0;
-
 // Assign GLOBAL variables
+let totalScore = 0;
 let scoreToBeat = 0;
 let wins = 0;
 let loses = 0;
@@ -13,11 +11,20 @@ function randNumber(num1, num2) {
     return Math.floor((Math.random() * num1) + num2);
 }
 
+function enableClicks() {
+   $(".crystals").prop("disable", true);
+}
+
+function disableClicks() {
+   $(".crystals").prop("disbale", false);
+}
+
 // Start Function
 function start() {
-
+    enableClicks();
+    
     // Generate Random Number and assign it to a variable between 19 and 120 formula:[ () * (max - min + 1)) + min];
-    randNum = randNumber(120, 19)
+    randNum = randNumber(102, 19)
     console.log(randNum); //sanity test: works!
 
     // Display random number in "#scoreToBeat" ID.
@@ -31,7 +38,7 @@ function start() {
 
     // let saphireNum = Math.floor((Math.random() * 12) + 1);
     // console.log(saphireNum);
-    // let rubyNum = Math.floor((Math.random() * 12) + 1);jhsdjsyurrfeh5ewyrchfykgxyjfhgf  vcmcn cnxccbznsdn bxmzbmbnbnasmnxmabxmabbscdhgsjxhsghxasghnxagmdhgwhxhwdvhgh
+    // let rubyNum = Math.floor((Math.random() * 12) + 1);
     // console.log(rubyNum);
     // let diamondNum = Math.floor((Math.random() * 12) + 1);
     // console.log(diamondNum);
@@ -42,7 +49,7 @@ function start() {
     // return [saphireNum, rubyNum, diamondNum, amethystNum];
 
     //---------------------------
-    //THIS IS THE NEW CODE
+    //THIS IS THE NEW CODE implementing the randNumber function.
     //---------------------------
 
     let saphireNum = randNumber(12, 1);
@@ -54,7 +61,10 @@ function start() {
     let amethystNum = randNumber(12, 1);
     console.log(amethystNum);
     totalScore = 0;
+
+    // Display the score clicked by the gem on "#totalScore" html.
     $("#totalScore").text(totalScore);
+    //returns the vallue of all gems in an array into the start() function.
     return [saphireNum, rubyNum, diamondNum, amethystNum];
 }
 
@@ -76,19 +86,19 @@ $(".crystals").on("click", function () {
     $("#totalScore").text(totalScore);
     console.log(totalScore); //sanity check: works!
 
-    // If totalScore === scoreToBeat add +1 to "winsCoount
+    // If totalScore === scoreToBeat add +1 to "winsCount
     if (totalScore === randNum) {
+        disableClicks();
         wins++;
-        $(this).unbind('click');
         setTimeout(() => {
             $("#winsCount").text(wins);
             gemValues = start();
-        }, 1500);
+        }, 1000);
 
         // else +1 to losesCount
     } else if (totalScore > randNum) {
+        disableClicks();
         loses++;
-        $(this).unbind('click');
         setTimeout(() => {
             $("#losesCount").text(loses);
             gemValues = start();

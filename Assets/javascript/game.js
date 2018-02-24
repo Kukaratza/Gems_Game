638dfb3cdcd22365bc55bb7256bf49977066b909
Kukaratza <marioandres.martinez80@gmail.com>
2018-02-23 22:14:50 -0800

    IT WORKSgit statusgit status!

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -11,18 +11,17 @@ function randNumber(num1, num2) {
     return Math.floor((Math.random() * num1) + num2);
 }
 
-function enableClicks() {
-   $(".crystals").prop("disable", true);
-}
+// function enableClicks() {
+//    $(".crystals").prop("disable", true);
+// }
 
-function disableClicks() {
-   $(".crystals").prop("disbale", false);
-}
+// function disableClicks() {
+//    $(".crystals").prop("disbale", false);
+// }
 
 // Start Function
 function start() {
-    enableClicks();
-    
+
     // Generate Random Number and assign it to a variable between 19 and 120 formula:[ () * (max - min + 1)) + min];
     randNum = randNumber(102, 19)
     console.log(randNum); //sanity test: works!
@@ -65,47 +64,50 @@ function start() {
     // Display the score clicked by the gem on "#totalScore" html.
     $("#totalScore").text(totalScore);
     //returns the vallue of all gems in an array into the start() function.
+    enableClicks();
     return [saphireNum, rubyNum, diamondNum, amethystNum];
 }
 
 // Create an on.click event that triggers and adds the "saphireNum" to the totalScore.
-$(".crystals").on("click", function () {
+function enableClicks() {
 
-    // if .this "click" event .is(has) id "img-saphire"
-    if ($(this).is("#img-saphire")) {
-        //add the number gen by saphire to totalScore, rinse and repeat...
-        totalScore += gemValues[0];
-    } else if ($(this).is("#img-ruby")) {
-        totalScore += gemValues[1];
-    } else if ($(this).is("#img-diamond")) {
-        totalScore += gemValues[2];
-    } else if ($(this).is("#img-amethyst")) {
-        totalScore += gemValues[3];
-    }
+    $(".crystals").on("click", function () {
+        // if .this "click" event .is(has) id "img-saphire"
+        if ($(this).is("#img-saphire")) {
+            //add the number gen by saphire to totalScore, rinse and repeat...
+            totalScore += gemValues[0];
+        } else if ($(this).is("#img-ruby")) {
+            totalScore += gemValues[1];
+        } else if ($(this).is("#img-diamond")) {
+            totalScore += gemValues[2];
+        } else if ($(this).is("#img-amethyst")) {
+            totalScore += gemValues[3];
+        }
 
-    $("#totalScore").text(totalScore);
-    console.log(totalScore); //sanity check: works!
+        $("#totalScore").text(totalScore);
+        console.log(totalScore); //sanity check: works!
 
-    // If totalScore === scoreToBeat add +1 to "winsCount
-    if (totalScore === randNum) {
-        disableClicks();
-        wins++;
-        setTimeout(() => {
-            $("#winsCount").text(wins);
-            gemValues = start();
-        }, 1000);
+        // If totalScore === scoreToBeat add +1 to "winsCount
+        if (totalScore === randNum) {
+            $('.crystals').off();
+            wins++;
+            setTimeout(() => {
+                $("#winsCount").text(wins);
+                gemValues = start();
+            }, 1000);
 
-        // else +1 to losesCount
-    } else if (totalScore > randNum) {
-        disableClicks();
-        loses++;
-        setTimeout(() => {
-            $("#losesCount").text(loses);
-            gemValues = start();
-        }, 1000);
+            // else +1 to losesCount
+        } else if (totalScore > randNum) {
+            $('.crystals').off();
+            loses++;
+            setTimeout(() => {
+                $("#losesCount").text(loses);
+                gemValues = start();
+            }, 1000);
 
-    }
-});
+        }
+    });
+}
 gemValues = start();
 console.log(gemValues);
 
